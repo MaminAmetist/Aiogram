f0bb2d7a89ac905f7849f9ed846275aa4dfc4804
Анна Нездулькина <anna.nezdulkina@mail.ru>
2025-01-14 20:54:35 +0700

    Create lexicon.py

---
+++ lexicon/lexicon.py
@@ -0,0 +1,22 @@
+MAIN_MENU: dict[str, str] = {
+    'menu_1': 'Добавить запись',
+    'menu_2': 'Список покупок',
+    'menu_3': 'Мероприятия'
+}
+
+INLINE_BUTTON: dict[str, str] = {
+    'add_1': 'Добавить запись в список',
+    'add_2': 'Добавить мероприятие'
+}
+
+INLINE_BUTTON_ADMIN: dict[str, str] = {
+    'admin_1': 'Рассылка',
+    'admin_2': 'Статистика',
+    'admin_3': 'Казнить',
+    'admin_4': 'Миловать',
+}
+
+INLINE_BUTTON_STORE: dict[str, str] = {
+    'add_3': 'Да',
+    'add_4': 'Нет'
+}
